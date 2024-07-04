8ec73fd80e658c4cb7daffea5a486da88adfaf26
Mateo Avantaggiato <mate.qac@gmail.com>
2024-07-04 12:25:46 +0200

    Refactor rental class and rental spec to satisfy level 2 solution

--- backend/models/rental.rb
+++ backend/models/rental.rb
@@ -16,8 +16,25 @@ class Rental
   def price(cars)
     car = cars.find { |car| car.id == car_id }
     rental_days = (Date.parse(end_date) - Date.parse(start_date)).to_i + 1
-    time_component = car.price_per_day * rental_days
+
+    time_component = rental_days.times.reduce(0) do |sum, day|
+      sum + (car.price_per_day * (1 - discount(day + 1)))
+    end
     distance_component = car.price_per_km * distance
     time_component + distance_component
+  end
+
+  private
+
+  def discount(rental_days)
+    if rental_days > 10
+      0.5
+    elsif rental_days > 4
+      0.3
+    elsif rental_days > 1
+      0.1
+    else
+      0
+    end
   end
 end
--- backend/spec/models/rental_spec.rb
+++ backend/spec/models/rental_spec.rb
@@ -21,12 +21,35 @@ RSpec.describe Rental do
   end
 
   describe '#price' do
-    context 'with valid input' do
-      it 'returns the price of the right rental' do
-        cars = [Car.new(id: 1, price_per_day: 2000, price_per_km: 10),
-                Car.new(id: 2, price_per_day: 3000, price_per_km: 15)]
-        rental = Rental.new(id: 1, car_id: 1, start_date: '2017-12-8', end_date: '2017-12-10', distance: 100)
-        expect(rental.price(cars)).to eq 7000
+    let(:cars) do
+      [Car.new(id: 1, price_per_day: 2000, price_per_km: 10),
+       Car.new(id: 2, price_per_day: 3000, price_per_km: 15)]
+    end
+    context 'when rental days is 1' do
+      it 'does not apply any discount' do
+        rental = Rental.new(id: 1, car_id: 1, start_date: '2017-12-08', end_date: '2017-12-08', distance: 100)
+        expect(rental.price(cars)).to eq 3000
+      end
+    end
+
+    context 'when rental days are between 2 and 4' do
+      it 'applies a 10% discount on the price per day' do
+        rental = Rental.new(id: 1, car_id: 1, start_date: '2017-12-08', end_date: '2017-12-09', distance: 300)
+        expect(rental.price(cars)).to eq 6800
+      end
+    end
+
+    context 'when rental days are between 5 and 10' do
+      it 'applies a 30% discount on the price per day' do
+        rental = Rental.new(id: 1, car_id: 1, start_date: '2017-12-8', end_date: '2017-12-14', distance: 100)
+        expect(rental.price(cars)).to eq 12_600
+      end
+    end
+
+    context 'when rental days are more than 10' do
+      it 'applies a 50% discount on the price per day' do
+        rental = Rental.new(id: 1, car_id: 1, start_date: '2017-12-03', end_date: '2017-12-14', distance: 1000)
+        expect(rental.price(cars)).to eq 27_800
       end
     end
   end
